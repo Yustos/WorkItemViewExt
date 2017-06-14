1947f21a767ea24941a5c637c6b1c8344f96604a
Yustos <yustos@inbox.ru>
2017-06-14 15:07:20 +0300

    Package id changed.

--- WorkItemViewExt/Guids.cs
+++ WorkItemViewExt/Guids.cs
@@ -6,8 +6,8 @@ namespace YL.WorkItemViewExt
 {
     static class GuidList
     {
-        public const string guidWorkItemViewExtPkgString = "af4a407a-3486-40c5-975d-9dde883ff0b5";
-        public const string guidWorkItemViewExtCmdSetString = "106d7db2-7b92-442f-92d4-96d229406300";
+        public const string guidWorkItemViewExtPkgString = "8f069d8f-506e-461f-a3d6-e2a76e1e1a8f";
+        public const string guidWorkItemViewExtCmdSetString = "68454935-794a-4744-a2ee-20a3b2889a80";
 
         public static readonly Guid guidWorkItemViewExtCmdSet = new Guid(guidWorkItemViewExtCmdSetString);
     };
